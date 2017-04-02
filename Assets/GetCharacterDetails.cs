8c1874d6ba5d12ab23926e7562993feeb4fcec4d
Patrick Johansen <Patrick Johansen>
2017-04-02 20:40:18 +0200

    Updated character creation

--- Assets/GetCharacterDetails.cs
+++ Assets/GetCharacterDetails.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GetCharacterDetails : MonoBehaviour {
+public class GetCharacterDetails : MonoBehaviour
+{
 
     public PlayerCharacter pc;
     public Text details;
 
-    public void Start() {
-        
-    }
+    public void Start()
+    {
 
-    public void UpdateText() {
-        Debug.Log(pc.GetFetish());
-        details.text = " Name: " +  pc.GetName() + "\n Age: " + pc.GetAge() + "\n Gender: " + pc.GetGender() + 
-            "\n Former Occupation: " + pc.GetOccupation() + "\n Creed: \n " + pc.GetCreed() + "\n Fetish: " + pc.GetFetish() +
-            "\n Alignment: " + pc.alignment;
     }
 
 
+    public void UpdateText()
+    {
+        Debug.Log("Calling method");
+        details.text = 
+            "\n \n Name: " + pc.GetName() + "\n \n Age: " + pc.GetAge() + "\n \n Gender: " + pc.GetGender() +
+            "\n \n Former Occupation: " + pc.GetOccupation() + "\n \n Creed: \n \n " + pc.GetCreed() + "\n \n Fetish: " + pc.GetFetish() +
+            "\n \n Alignment: " + pc.alignment +
+            "\n \n Action Stats [10 : 100]: " +
+            "\n  STRENGTH: " + pc.GetStr() +
+            "\n  ENDURANCE: " + pc.GetEnd() +
+            "\n  PERSUASION: " + pc.GetPer() +
+            "\n  ALLURE: " + pc.GetAll() +
+            "\n  SWIFTNESS: " + pc.GetSwf() +
+            "\n  WILLPOWER: " + pc.GetWP();
+
+    }
 }
 
--- Assets/PlayerCharacter.cs
+++ Assets/PlayerCharacter.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour {
+public class PlayerCharacter : MonoBehaviour
+{
 
     // Basic
-     string playerName;
-     int age;
-     string gender;
-     string former_occupation;
-     string creed;
-     string fetish;
+    string playerName;
+    int age;
+    string gender;
+    string former_occupation;
+    string creed;
+    string fetish;
 
     // Action Stats
-    int strength;
-    int willpower;
-    int persuasion;
-    int endurance;
-    int allure;
-    int swiftness;
+    int str;
+    int wp;
+    int per;
+    int end;
+    int all;
+    int swf;
     int maxHP;
     int curHP;
     int maxWill;
@@ -26,30 +27,35 @@ public class PlayerCharacter : MonoBehaviour {
 
     // Personality Stats
     int alignmentPoints;
-    public enum Alignment { Wicked, Douchebag, Neutral, Honorable, Righteous};
+    public enum Alignment { Wicked, Douchebag, Neutral, Honorable, Righteous };
     public Alignment alignment = Alignment.Neutral;
     int sexperience;
 
-    public void SetAlignmentPoints(int val) {
+    public void SetAlignmentPoints(int val)
+    {
         alignmentPoints = val;
         UpdateAlignment();
     }
 
-    public void IncreaseAlignment(int val) {
+    public void IncreaseAlignment(int val)
+    {
         alignmentPoints += val;
         UpdateAlignment();
     }
 
-    public void DecreaseAlignment(int val) {
+    public void DecreaseAlignment(int val)
+    {
         alignmentPoints -= val;
         UpdateAlignment();
     }
 
-    public int GetAlignmentPoints() {
+    public int GetAlignmentPoints()
+    {
         return alignmentPoints;
     }
 
-    void UpdateAlignment() {
+    void UpdateAlignment()
+    {
         if (alignmentPoints <= 10)
         {
             alignment = Alignment.Wicked;
@@ -66,50 +72,150 @@ public class PlayerCharacter : MonoBehaviour {
         {
             alignment = Alignment.Honorable;
         }
-        else if (alignmentPoints >= 90) {
+        else if (alignmentPoints >= 90)
+        {
             alignment = Alignment.Righteous;
         }
     }
 
-    public void SetName(string n) {
+    void CheckProfile(string occupation)
+    {
+        switch (occupation)
+        {
+            case "Mercenary":
+                str = 60;
+                end = 50;
+                all = 20;
+                wp = 20;
+                per = 10;
+                swf = 30;
+                break;
+            case "Blacksmith":
+                str = 50;
+                end = 60;
+                all = 20;
+                wp = 30;
+                per = 30;
+                swf = 10;
+                break;
+            case "Spy":
+                str = 10;
+                end = 40;
+                all = 40;
+                wp = 40;
+                per = 40;
+                swf = 40;
+                break;
+            case "Gigolo":
+                str = 20;
+                end = 30;
+                all = 20;
+                wp = 40;
+                per = 40;
+                swf = 50;
+                break;
+            case "Vagabond":
+                str = 60;
+                end = 50;
+                all = 20;
+                wp = 20;
+                per = 10;
+                swf = 30;
+                break;
+            case "Diplomat":
+                str = 10;
+                end = 30;
+                all = 40;
+                wp = 50;
+                per = 60;
+                swf = 10;
+                break;
+            case "Hunter":
+                str = 20;
+                end = 40;
+                all = 30;
+                wp = 40;
+                per = 10;
+                swf = 60;
+                break;
+            case "Squire":
+                str = 60;
+                end = 50;
+                all = 20;
+                wp = 20;
+                per = 10;
+                swf = 30;
+                break;
+            case "Missionary":
+                str = 30;
+                end = 10;
+                all = 20;
+                wp = 60;
+                per = 60;
+                swf = 20;
+                break;
+            case "Merchant":
+                str = 10;
+                end = 30;
+                all = 40;
+                wp = 40;
+                per = 60;
+                swf = 30;
+                break;
+        }
+    }
+
+    public void SetName(string n)
+    {
         playerName = n;
     }
 
-    public string GetName() {
+    public string GetName()
+    {
         return playerName;
     }
 
-    public void SetAge(int a) {
+    public void SetAge(int a)
+    {
         age = a;
     }
 
-    public int GetAge() {
+    public int GetAge()
+    {
         return age;
     }
 
-    public void SetGender(string g) {
+    public void SetGender(string g)
+    {
         gender = g;
     }
 
-    public string GetGender() {
+    public string GetGender()
+    {
         return gender;
     }
 
-    public void SetOccupation(string fo) {
+    public void SetOccupation(string fo)
+    {
         former_occupation = fo;
+        CheckProfile(former_occupation);
     }
 
-    public string GetOccupation() {
+    public string GetOccupation()
+    {
         return former_occupation;
     }
 
-    public void SetCreed(string c) {
+    public void SetCreed(string c)
+    {
         creed = c;
         CheckCreed(creed);
     }
 
-    void CheckCreed(string c) {
-        switch (c) {
+    void CheckCreed(string c)
+    {
+        switch (c)
+        {
             case "I will have it my way - or it won’t be let to exist at all":
                 SetAlignmentPoints(30);
                 break;
@@ -143,15 +249,48 @@ public class PlayerCharacter : MonoBehaviour {
         }
     }
 
-    public string GetCreed() {
+    public string GetCreed()
+    {
         return creed;
     }
 
-    public void SetFetish(string f) {
+    public void SetFetish(string f)
+    {
         fetish = f;
     }
 
-    public string GetFetish() {
+    public string GetFetish()
+    {
         return fetish;
+    }
+
+    public int GetStr()
+    {
+        return str;
+    }
+
+    public int GetEnd()
+    {
+        return end;
+    }
+
+    public int GetPer()
+    {
+        return per;
+    }
+
+    public int GetAll()
+    {
+        return all;
+    }
+
+    public int GetSwf()
+    {
+        return swf;
+    }
+
+    public int GetWP()
+    {
+        return wp;
     }
 }
